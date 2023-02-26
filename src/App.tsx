5458afd6f207a11970e1cb27a2116600aa948e27
afrangyan <afrangyan@kameleoon.com>
2023-02-27 00:00:10 +0300

    created navigation and routing. project completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,19 @@
-import { useState } from "react";
-import { CreateProduct } from "./components/CreateProduct";
-import ErrorMessage from "./components/ErrorMessage";
-import Loader from "./components/Loader";
-import Modal from "./components/Modal";
-import { Product } from "./components/Product";
-import { useProducts } from "./hooks/products";
-import { IProduct } from "./models";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import { AboutPage } from "./pages/AboutPage";
+import ProductsPage from "./pages/ProductsPage";
 
 function App() {
-  const { loading, error, products, addProduct } = useProducts();
-  const [modal, setModal] = useState(true);
-
-  const createHandler = (product: IProduct) => {
-    setModal(false);
-    addProduct(product);
-  };
-
   return (
-    <div className="container mx-auto max-w-2-xl pt-5">
-      {loading && <Loader />}
-      {error && <ErrorMessage error={error} />}
-      {products.map((product) => (
-        <Product product={product} key={product.id} />
-      ))}
-
-      {modal && (
-        <Modal title="Create new product" onClose={() => setModal(false)}>
-          <CreateProduct onCreate={createHandler} />
-        </Modal>
-      )}
-    </div>
-  );
+  <>
+    <Navigation/>
+    
+    <Routes>
+      <Route path="/" element={ <ProductsPage /> }/>
+      <Route path="/about" element={ < AboutPage /> }/>
+    </Routes>
+  </>
+  )
 }
 
 export default App;
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+export function AboutPage () {
+   return (
+     <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga laboriosam delectus a omnis earum perferendis, amet, sunt, dolor eaque commodi eius neque quibusdam? Velit nisi beatae nam voluptatem quis, repellat impedit deleniti quos ipsum molestias architecto exercitationem quo molestiae sed temporibus. Provident ducimus sit voluptatum cupiditate, ut repellendus nam. Ex.</div>
+   )
+}
